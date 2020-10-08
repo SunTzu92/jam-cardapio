25d9fc6ea325280064a342f53460a9c57bc796b3
Lucas Thibau <dev16.osasbh@outlook.com>
2020-10-08 02:29:04 -0300

    métodos para marcar e desmarcar menu

--- src/components/Legenda/index.js
+++ src/components/Legenda/index.js
@@ -1,4 +1,4 @@
-﻿import React from 'react'
+﻿import React, { useCallback, useState } from 'react'
 import { WiRaindrop } from 'react-icons/wi'
 import { AiFillApple, AiFillStar } from 'react-icons/ai'
 
@@ -8,29 +8,70 @@ import IconNoGluten from '../Icons/NoGluten'
 import * as S from './styles'
 
 const Legenda = () => {
+  const [active, setActive] = useState({
+    favoritos: true,
+    usarShoyu: true,
+    vegetariano: true,
+    semLactose: true,
+    semGluten: true,
+    meiaPorcao: true
+  })
+
+  const clicaLegenda = useCallback(
+    (target, event) => {
+      active[target] = !active[target]
+      setActive(active)
+    },
+    [active]
+  )
+
   return (
     <S.Container>
-      <S.Item mLeft="0.25rem">
+      <S.Item
+        mLeft="0.25rem"
+        onClick={clicaLegenda.bind(this, 'favoritos')}
+        active={active['favoritos']}
+      >
         <AiFillStar size={15} className="icon--ai" />
         <span>Favoritos</span>
       </S.Item>
-      <S.Item mLeft="-2px">
+      <S.Item
+        mLeft="-2px"
+        onClick={clicaLegenda.bind(this, 'usarShoyu')}
+        active={active['usarShoyu']}
+      >
         <WiRaindrop size={25} />
         <span>Usar Shoyu</span>
       </S.Item>
-      <S.Item mLeft="0.25rem">
+      <S.Item
+        mLeft="0.25rem"
+        onClick={clicaLegenda.bind(this, 'vegetariano')}
+        active={active['vegetariano']}
+      >
         <AiFillApple size={15} className="icon--ai" />
         <span>Vegetariano</span>
       </S.Item>
-      <S.Item mLeft="0.25rem">
+      <S.Item
+        mLeft="0.25rem"
+        onClick={clicaLegenda.bind(this, 'semLactose')}
+        active={active['semLactose']}
+      >
         <IconNoLactose />
         <span>Sem Lactose</span>
       </S.Item>
-      <S.Item mLeft="0.25rem">
+      <S.Item
+        mLeft="0.25rem"
+        onClick={clicaLegenda.bind(this, 'semGluten')}
+        active={active['semGluten']}
+      >
         <IconNoGluten />
         <span>Sem Glúten</span>
       </S.Item>
-      <S.Item mLeft="0.25rem">
+      <S.Item
+        mLeft="0.25rem"
+        onClick={clicaLegenda.bind(this, 'meiaPorcao')}
+        active={active['meiaPorcao']}
+      >
         <span>Meia Porção</span>
       </S.Item>
     </S.Container>
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,57 @@
-﻿import React from 'react'
+﻿import React, { useCallback, useState } from 'react'
 
 import * as S from './styles'
 
 function Menu() {
+  const [active, setActive] = useState({
+    entradas: true,
+    principais: true,
+    sushi: true,
+    sobremesas: true
+  })
+
+  const clicaMenu = useCallback(
+    (target, event) => {
+      console.log('target: ', target)
+      console.log('active: ', active)
+      console.log('sushi: ', active['sushi'])
+      console.log('active_target: ', active[target])
+      console.log('--------------------')
+      active[target] = !active[target]
+      setActive(active)
+    },
+    [active]
+  )
+
   return (
     <S.Container>
-      <S.Button type="button" active={true}>
+      <S.Button
+        type="button"
+        onClick={clicaMenu.bind(this, 'entradas')}
+        active={active['entradas']}
+      >
         Entradas
       </S.Button>
       <S.Divider />
-      <S.Button type="button">Pratos Principais</S.Button>
+      <S.Button
+        type="button"
+        onClick={clicaMenu.bind(this, 'principais')}
+        active={active['principais']}
+      >
+        Pratos Principais
+      </S.Button>
       <S.Divider />
-      <S.Button type="button">Sushi {'&'} Roll</S.Button>
+      <S.Button type="button" onClick={clicaMenu.bind(this, 'sushi')} active={active['sushi']}>
+        Sushi {'&'} Roll
+      </S.Button>
       <S.Divider />
-      <S.Button type="button">Sobremesas</S.Button>
+      <S.Button
+        type="button"
+        onClick={clicaMenu.bind(this, 'sobremesas')}
+        active={active['sobremesas']}
+      >
+        Sobremesas
+      </S.Button>
     </S.Container>
   )
 }
