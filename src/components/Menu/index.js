ad1fc4322fee5941386633b71f04eec0602ba2d9
Michael Douglas <dev19.osasbh@outlook.com>
2020-10-08 19:00:58 -0300

    fix(logic): correção da logica ao selecionar um item

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,53 +3,39 @@
 import * as S from './styles'
 
 function Menu() {
-  const [active, setActive] = useState({
-    entradas: true,
-    principais: true,
-    sushi: true,
-    sobremesas: true
-  })
+  const [active, setActive] = useState('entradas')
 
-  const clicaMenu = useCallback(
-    (target, event) => {
-      console.log('target: ', target)
-      console.log('active: ', active)
-      console.log('sushi: ', active['sushi'])
-      console.log('active_target: ', active[target])
-      console.log('--------------------')
-      active[target] = !active[target]
-      setActive(active)
-    },
-    [active]
-  )
+  const onClickItem = useCallback((target, event) => {
+    setActive(target)
+  }, [])
 
   return (
     <S.Container>
       <S.Button
         type="button"
-        onClick={clicaMenu.bind(this, 'entradas')}
-        active={active['entradas']}
-      >
+        onClick={onClickItem.bind(this, 'entradas')}
+        active={active === 'entradas'}>
         Entradas
       </S.Button>
       <S.Divider />
       <S.Button
         type="button"
-        onClick={clicaMenu.bind(this, 'principais')}
-        active={active['principais']}
-      >
+        onClick={onClickItem.bind(this, 'principais')}
+        active={active === 'principais'}>
         Pratos Principais
       </S.Button>
       <S.Divider />
-      <S.Button type="button" onClick={clicaMenu.bind(this, 'sushi')} active={active['sushi']}>
+      <S.Button
+        type="button"
+        onClick={onClickItem.bind(this, 'sushi')}
+        active={active === 'sushi'}>
         Sushi {'&'} Roll
       </S.Button>
       <S.Divider />
       <S.Button
         type="button"
-        onClick={clicaMenu.bind(this, 'sobremesas')}
-        active={active['sobremesas']}
-      >
+        onClick={onClickItem.bind(this, 'sobremesas')}
+        active={active === 'sobremesas'}>
         Sobremesas
       </S.Button>
     </S.Container>
