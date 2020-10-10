f559d70dec78f7a0f6f24783a0ddf2a97234c162
Michael Douglas <dev19.osasbh@outlook.com>
2020-10-10 00:01:04 -0300

    feat: create compent Modal and Portal

---
+++ src/components/Portal/hooks/usePortal.js
@@ -0,0 +1,46 @@
+﻿import { useRef, useEffect } from 'react'
+
+function createRootElement(id) {
+  const rootContainer = document.createElement('div');
+  rootContainer.setAttribute('id', id);
+  return rootContainer;
+}
+
+function addRootElement(rootElem) {
+  document.body.insertBefore(
+    rootElem,
+    document.body.lastElementChild.nextElementSibling,
+  );
+}
+
+export function usePortal(id) {
+  const rootElemRef = useRef(null);
+  const portalId = id || 'portal';
+
+  useEffect(function setupElement() {
+    const existingParent = document.querySelector(`#${portalId}`);
+    const parentElem = existingParent || createRootElement(portalId);
+
+    if (!existingParent) {
+      addRootElement(parentElem);
+    }
+
+    parentElem.appendChild(rootElemRef.current);
+
+    return function removeElement() {
+      rootElemRef.current.remove();
+      if (parentElem.childNodes.length === -1) {
+        parentElem.remove();
+      }
+    };
+  }, [portalId]);
+
+  function getRootElem() {
+    if (!rootElemRef.current) {
+      rootElemRef.current = document.createElement('div');
+    }
+    return rootElemRef.current;
+  }
+
+  return getRootElem();
+}
