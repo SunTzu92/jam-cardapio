1219ed1c96e234908ed8b035f325346b9473026f
Michael Douglas <dev19.osasbh@outlook.com>
2020-10-10 21:48:15 -0300

    feat(logic): legenda implementada

---
+++ src/components/Legenda/Item.jsx
@@ -0,0 +1,31 @@
+﻿import React from 'react'
+
+import IconFavoritos from '../Icons/Favoritos'
+import IconShoyu from '../Icons/Shoyu'
+import IconVegano from '../Icons/Vegano'
+import IconNoLactose from '../Icons/NoLactose'
+import IconNoGluten from '../Icons/NoGluten'
+import IconMeiaPorcao from '../Icons/MeiaPorcao'
+
+import * as S from './styles'
+
+const tipoIcone = {
+  favorito: IconFavoritos,
+  usarShoyu: IconShoyu,
+  vegano: IconVegano,
+  semLactose: IconNoLactose,
+  semGluten: IconNoGluten,
+  meiaPorcao: IconMeiaPorcao
+}
+
+const Item = ({ title, name, active, onClick }) => {
+  const Icon = tipoIcone[name]
+  return (
+    <S.Item onClick={(event) => onClick(name)} checked={active}>
+      <span>{<Icon />}</span>
+      {title}
+    </S.Item>
+  )
+}
+
+export default Item
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,11 +1,13 @@
 ﻿import { combineReducers } from 'redux'
 
 import menuReducer from './menuReducer'
+import legendaReducer from './legendaReducer'
 import bebidasReducer from './bebidaReducer'
 import comidasReducer from './comidaReducer'
 
 const rootReducer = combineReducers({
   menu: menuReducer,
+  legenda: legendaReducer,
   bebidas: bebidasReducer,
   comidas: comidasReducer
 })
